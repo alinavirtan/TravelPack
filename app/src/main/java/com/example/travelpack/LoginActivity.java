c0e9460e6c3b367543329a0012469aba7ffe425c
alinavirtan <virtan_alinaa@yahoo.com>
2021-04-19 01:28:09 +0300

    Database sync

--- app/src/main/java/com/example/travelpack/LoginActivity.java
+++ app/src/main/java/com/example/travelpack/LoginActivity.java
@@ -1,36 +1,64 @@
 package com.example.travelpack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email, password;
+    TextView registerLink;
+    Button loginBtn;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView register = findViewById(R.id.gotoRegister);
-        register.setOnClickListener(new View.OnClickListener() {
+        registerLink = findViewById(R.id.gotoRegister);
+        loginBtn = findViewById(R.id.btnLogin);
+        email = findViewById(R.id.inputEmail);
+        password = findViewById(R.id.inputPassword);
+        DB = new DBHelper(this);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
-                //setContentView(R.layout.activity_register);
             }
         });
 
-        Button btn = findViewById(R.id.btnLogin);
-        btn.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, AddTripActivity.class);
-                startActivity(intent);
-                //setContentView(R.layout.activity_add_trip);
+//                Intent intent = new Intent(LoginActivity.this, AddTripActivity.class);
+//                startActivity(intent);
+
+                String user_email = email.getText().toString();
+                String user_pass = password.getText().toString();
+
+                if (user_email.equals("") || user_pass.equals("")) {
+                    Toast.makeText(LoginActivity.this, "Please enter all the field", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkuserpass = DB.checkEmailPassword(user_email, user_pass);
+
+                    if (checkuserpass == true) {
+                        Toast.makeText(LoginActivity.this, "Sign in successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, AddTripActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
--- app/src/main/java/com/example/travelpack/RegisterActivity.java
+++ app/src/main/java/com/example/travelpack/RegisterActivity.java
@@ -2,39 +2,70 @@ package com.example.travelpack;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText name, email, password;
+    TextView loginLink;
+    Button signupBtn;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        loginLink = findViewById(R.id.gotoLogin);
+        signupBtn = findViewById(R.id.btnSignup);
+        name = findViewById(R.id.inputName);
+        email = findViewById(R.id.inputEmailAddr);
+        password = findViewById(R.id.inputPasswd);
+        DB = new DBHelper(this);
 
-        TextView login = findViewById(R.id.gotoLogin);
-        login.setOnClickListener(new View.OnClickListener() {
+        loginLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
-               //setContentView(R.layout.activity_login);
             }
         });
 
 
-        Button btn = findViewById(R.id.btnSignup);
-        btn.setOnClickListener(new View.OnClickListener() {
+        signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
-                //setContentView(R.layout.activity_login);
+//                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+//                startActivity(intent);
+
+                String user_email = email.getText().toString();
+                String user_password = password.getText().toString();
+                String user_name = name.getText().toString();
+
+                if (user_email.equals("") || user_password.equals("") || user_name.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkuser = DB.checkEmail(user_email);
+
+                    if (checkuser == false) {
+                        Boolean insert = DB.insertData(user_email, user_name, user_password);
+
+                        if (insert == true) {
+                            Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "User already exists! Please sign in.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
