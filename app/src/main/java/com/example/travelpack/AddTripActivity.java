3715459ba74ed8c8dc6795be40785ffd75b9a935
Florentina <hertigflorentina@gmail.com>
2021-05-06 00:48:09 +0300

    Adaugare introducere destinatie

--- app/src/main/java/com/example/travelpack/AddTripActivity.java
+++ app/src/main/java/com/example/travelpack/AddTripActivity.java
@@ -1,13 +1,72 @@
 package com.example.travelpack;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class AddTripActivity extends AppCompatActivity {
+    EditText editText;
+    EditText date;
+    DatePickerDialog.OnDateSetListener setListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trip);
+
+        editText = findViewById(R.id.inputDestination);
+        date = findViewById(R.id.inputDate1);
+
+        String apiKey = getString(R.string.apiKey);
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+
+        editText.setFocusable(false);
+        editText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                List<Place.Field> fieldList = Arrays.asList(Place.Field.ID,
+                        Place.Field.NAME);
+
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,
+                        fieldList).build(AddTripActivity.this);
+
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            Place place = Autocomplete.getPlaceFromIntent(data);
+
+            editText.setText(place.getName());
+        } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+            Status status = Autocomplete.getStatusFromIntent(data);
+
+            Toast.makeText(getApplicationContext(), status.getStatusMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
